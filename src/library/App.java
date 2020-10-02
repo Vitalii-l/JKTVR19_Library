4e96c227cdc99869ecc6b8b0f0a7b735f81b0e8f
pupil <pupil@ivkhk.ee>
2020-10-02 15:20:48 +0300

    Созданы сущности Книга, Читатель, добавлена книга, читатель, вывод списка читателей

--- src/library/App.java
+++ src/library/App.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package library;
+import entity.Book;
 import java.util.Scanner;
 
 /**
@@ -12,46 +13,69 @@ import java.util.Scanner;
  */
 class App {
     private Scanner scanner = new Scanner(System.in);
+    private Reader[] readers = new Reader[10];
+    
+    
     public void run() {
         System.out.println("--- Library");
         System.out.println("Menu:");
         boolean repeat = true;
-        System.out.println("\n1. Add book");
-        System.out.println("2. List of books");
-        System.out.println("3. Give a book");        
-        System.out.println("4. Take a book");
-        System.out.println("5. Add readrer");
-        System.out.println("6. List of readers");
-        System.out.println("0. Exit");
-        String task = scanner.nextLine();
         
         do {
+            System.out.println("\n1. Add book");
+            System.out.println("2. List of books");
+            System.out.println("3. Give a book");        
+            System.out.println("4. Take a book");
+            System.out.println("5. Add readrer");
+            System.out.println("6. List of readers");
+            System.out.println("0. Exit");
+            System.out.println("");
+            String task = scanner.nextLine();
             switch (task){
                 case "0":
                     System.out.println("--- The End ---");
                     repeat = false;
-                break;
+                    break;
                 case "1":
-                    System.out.println("1. Add book");
-                break;
+                    System.out.println("--- 1. Add book ---");
+                    Book book = new Book("Master i Magrarita", "M. Bulgakov", 2010);
+                    System.out.println("Book name:"+book.getName());
+                    System.out.println(book.toString());
+                    
+                    break;
                 case "2":
-                    System.out.println("2. List of books");
-                break;
+                    System.out.println("--- 2. List of books ---");
+                    break;
                 case "3":
-                    System.out.println("3. Give a book");
-                break;
+                    System.out.println("--- 3. Give a book ---");
+                    break;
                 case "4":
                     System.out.println("4. Take a book");
-                break;
+                    break;
                 case "5":
                     System.out.println("5. Add readrer");
-                break;
+                    Reader reader = new Reader("Zinaida", "Kavi", "57432209");
+                    readers[0] = reader;
+                    Reader reader1 = new Reader("Peter", "Pervyi","45589898");
+                    readers[1] = reader1;
+                    System.out.println("Reader: " + reader.getFirstName()+" "+reader.getLastName());
+                    System.out.println(reader.toString());
+                    
+                    break;
                 case "6":
                     System.out.println("6. List of readers");
-                break;
+                    int i = 0;
+                    for (Reader r : readers) {
+                        if(r != null){
+                            System.out.println(i+1+". "+r.toString());
+                            i++;
+                        }
+                    }
+                    
+                    break;
                 case "7":
 
-                break;            
+                    break;            
             } 
         }while (repeat);
 
---
+++ src/library/Reader.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+
+/**
+ *
+ * @author pupil
+ */
+public class Reader {
+    private String firstName;
+    private String lastName;
+    private String phone;
+
+    public Reader() {
+    }
+    public Reader(String firstName, String lastName, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Reader{" + "firstName=" + firstName + ", lastName=" + lastName + ", phone=" + phone + '}';
+    }
+    
+    
+}
